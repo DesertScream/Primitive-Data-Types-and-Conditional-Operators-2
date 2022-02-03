422a8333705fc8b59c7c6f09977eae123d17ad24
DesertScream <www.anton.dzuin@gmail.com>
2022-02-03 20:17:08 +0500

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+        int startingScore = 100;
+        int replenishmentAmount = 3000;
+        if (replenishmentAmount <= 1000) {
+            System.out.println("бонусов нет");
+        } else {
+            if (replenishmentAmount >= 1001) {
+                System.out.println("Бонус равен - " + replenishmentAmount / 100);
+                System.out.println("Итоговая сумма на счету клиента - " + (startingScore + replenishmentAmount));
+
+            }
+        }
+
+    }
 }
+//хотел через счетчик сделать, но понял , что не могу через Условия это реализовать(
